15426960758e972a1a37e19710d5c697e8c38586
samasultan <76969194+samasultan@users.noreply.github.com>
2021-05-02 15:01:50 -0700

    Add files via upload

---
+++ ass2/ass2/ass2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ass2
+//
+//  Created by user169313 on 5/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
